e69a23b6e4a59639277fd1a71053ba270f18073c
AllenK <revenoir7@gmail.com>
2019-09-22 23:02:07 +0900

    etc: lint 미적용파일 다시 적용

--- public/components/card_navigation/app.js
+++ public/components/card_navigation/app.js
@@ -1,27 +1,27 @@
-import {getJsonData} from "../../utils/allenibrary.js";
-import Templater from "./templater.js";
-import StateManager from "./state_manager.js";
-import Carousel from "./carousel.js";
-import Pagination from "./pagination.js";
-import config from "./config.js";
+import { getJsonData } from '../../utils/allenibrary.js';
+import Templater from './templater.js';
+import StateManager from './state_manager.js';
+import Carousel from './carousel.js';
+import Pagination from './pagination.js';
+import config from './config.js';
 
 const templater = new Templater();
-const stateManager = new StateManager({config});
-const pagination = new Pagination({stateManager, config});
-const carousel = new Carousel({stateManager, config});
+const stateManager = new StateManager({ config });
+const pagination = new Pagination({ stateManager, config });
+const carousel = new Carousel({ stateManager, config });
 
 const initCardNavigation = () => {
-	const data = getJsonData(config.url);
+  const data = getJsonData(config.url);
 
-	data
-		.then(data => templater.insertCards(data))
-		.then(data => {
-			const panelQuantity = data.length;
+  data
+    .then(data => templater.insertCards(data))
+    .then(data => {
+      const panelQuantity = data.length;
 
-			stateManager.init(panelQuantity);
-			pagination.init();
-			carousel.init();
-		});
+      stateManager.init(panelQuantity);
+      pagination.init();
+      carousel.init();
+    });
 };
 
 export default initCardNavigation;
